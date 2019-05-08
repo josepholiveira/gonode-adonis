8a0e71a033802b812b658e69c77cf4f370e0ae60
Joseph Oliveira <josephvalenetoliveira@gmail.com>
2019-05-08 20:50:54 -0300

    Adicionando fila de email com Redis e Kue

---
+++ app/Jobs/NewTaskMail.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const Mail = use('Mail')
+const Helpers = use('Helpers')
+
+class NewTaskMail {
+  static get concurrency () {
+    return 1
+  }
+
+  static get key () {
+    return 'NewTaskMail-job'
+  }
+
+  async handle ({ email, username, title, file }) {
+    console.log(`Job ${NewTaskMail.key}`)
+    await Mail.send(
+      ['emails.new_task'],
+      {
+        username,
+        title,
+        hasAttachment: !!file
+      },
+
+      message => {
+        message
+          .to(email)
+          .from('oi@josepholiveira.dev', 'Joseph Oliveira')
+          .subject('Nova tarefa para você')
+
+        if (file) {
+          message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
+            filename: file.name
+          })
+        }
+      }
+    )
+  }
+}
+
+module.exports = NewTaskMail
--- app/Models/Hooks/TaskHook.js
+++ app/Models/Hooks/TaskHook.js
@@ -1,7 +1,7 @@
 'use strict'
+const Kue = use('Kue')
+const Job = use('App/Jobs/NewTaskMail')
 
-const Mail = use('Mail')
-const Helpers = use('Helpers')
 const TaskHook = (exports = module.exports = {})
 TaskHook.sendNewTaskMail = async taskInstance => {
   if (!taskInstance.user_id && !taskInstance.dirty.user_id) return
@@ -10,25 +10,6 @@ TaskHook.sendNewTaskMail = async taskInstance => {
   const file = await taskInstance.file().fetch()
 
   const { title } = taskInstance
-  await Mail.send(
-    ['emails.new_task'],
-    {
-      username,
-      title,
-      hasAttachment: !!file
-    },
 
-    message => {
-      message
-        .to(email)
-        .from('oi@josepholiveira.dev', 'Joseph Oliveira')
-        .subject('Nova tarefa para você')
-
-      if (file) {
-        message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
-          filename: file.name
-        })
-      }
-    }
-  )
+  Kue.dispatch(Job.key, { email, username, file, title }, { attempts: 3 })
 }
