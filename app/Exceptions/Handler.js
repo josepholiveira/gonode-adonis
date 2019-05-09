229f2977fd5340c7df7aeda8469e9d4e605f21e5
Joseph Oliveira <josephvalenetoliveira@gmail.com>
2019-05-08 21:04:48 -0300

    Configurando Sentry para error reports

--- app/Exceptions/Handler.js
+++ app/Exceptions/Handler.js
@@ -1,4 +1,7 @@
 'use strict'
+
+const Raven = require('raven')
+const Config = use('Config')
 const Env = use('Env')
 const Youch = use('Youch')
 const BaseExceptionHandler = use('BaseExceptionHandler')
@@ -46,7 +49,9 @@ class ExceptionHandler extends BaseExceptionHandler {
    * @return {void}
    */
   async report (error, { request }) {
-    console.log(error)
+    Raven.config(Config.get(`services.sentry.dsn`))
+
+    Raven.captureException(error)
   }
 }
 
